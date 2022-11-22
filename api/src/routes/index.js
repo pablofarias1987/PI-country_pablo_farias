19ef3d6a9be1cc6d533c393b9d621c5b6782f71c
pablofarias1987 <pfgallardo1987@gmail.com>
2022-11-22 14:49:03 -0300

    2do commit back get hecho trae la api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+const { countryRoute } = require('./country');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+router.use('/countries', countryRoute)
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
